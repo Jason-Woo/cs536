0253dceb364f5c58c5c06502f4c2146433e29710
“YJason-Woo1 git config --global user.name “Jason-Woo <4everjasonwoo@gmail.com>
2020-11-21 22:13:47 -0500

    implement Perceptrons.py

--- regression/regression.py
+++ regression/regression.py
@@ -21,18 +21,19 @@ def regression(x, y):
 
 
 if __name__ == '__main__':
-    w_hat, w_hat_prime, b_hat, b_hat_prime = 0, 0, 0, 0
+    w_hat, w_hat_prime, b_hat, b_hat_prime = [], [], [], []
     for i in range(1000):
         x, x_prime, y = data_generate()
         w_hat_tmp, b_hat_tmp = regression(x, y)
-        w_hat += w_hat_tmp
-        b_hat += b_hat_tmp
+        w_hat.append(w_hat_tmp)
+        b_hat.append(b_hat_tmp)
         w_hat_tmp, b_hat_tmp = regression(x_prime, y)
-        w_hat_prime += w_hat_tmp
-        b_hat_prime += b_hat_tmp
-    w_hat /= 1000
-    b_hat /= 1000
-    w_hat_prime /= 1000
-    b_hat_prime /= 1000
-    print(w_hat, b_hat)
-    print(w_hat_prime, b_hat_prime)
+        w_hat_prime.append(w_hat_tmp)
+        b_hat_prime.append(b_hat_tmp)
+
+    print("Expected value")
+    print(np.mean(w_hat), np.mean(b_hat))
+    print(np.mean(w_hat_prime), np.mean(b_hat_prime))
+    print("variance")
+    print(np.var(w_hat), np.var(b_hat))
+    print(np.var(w_hat_prime), np.var(b_hat_prime))
