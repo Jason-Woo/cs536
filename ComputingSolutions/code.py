89c877179d6b59ead069efef82f1cd5b770e99bb
“YJason-Woo1 git config --global user.name “Jason-Woo <4everjasonwoo@gmail.com>
2020-11-29 18:43:24 -0500

    implement

--- ComputingSolutions/code.py
+++ ComputingSolutions/code.py
@@ -51,13 +51,13 @@ def naive_regression(x, y, gradient_descent=False):
 def ridge_regression(x, y, gradient_descent=False, lmb=0.1):
     # gradient_descent set to true to use gradient descent
     # algebraic solution set to false for algebraic solution
+    m, n = x.shape
     if gradient_descent:
-        m = x.shape[0]
-        w = np.zeros(21)
+        w = np.zeros(n)
         iter_cnt = 0
         learning_rate = 0.009
         err_prev = 9999999
-        threshold = 1e-30
+        threshold = 1e-10
 
         while True:
             y_predict = x.dot(w)
@@ -74,7 +74,7 @@ def ridge_regression(x, y, gradient_descent=False, lmb=0.1):
     else:
         # algebraic solution
         xtx = x.transpose().dot(x)
-        return np.linalg.inv(xtx + lmb * np.identity(21)).dot(x.transpose()).dot(y)
+        return np.linalg.inv(xtx + lmb * np.identity(n)).dot(x.transpose()).dot(y)
 
 
 def lasso_regression(x, y, lmb=0.01):
@@ -82,7 +82,7 @@ def lasso_regression(x, y, lmb=0.01):
     w = np.zeros(21)
     iter_cnt = 0
     err_prev = 9999999
-    threshold = 1e-7
+    threshold = 1e-3
 
     while True:
         y_predict = x.dot(w)
@@ -90,13 +90,11 @@ def lasso_regression(x, y, lmb=0.01):
         if err_prev - err < threshold:
             return w
         err_prev = err
-        if iter_cnt % 10 == 0:
+        if iter_cnt % 100 == 0:
             print(iter_cnt, err_prev)
         for k in range(21):
             z_k = (np.transpose(x[:, k]) * x[:, k]).sum()
-            w_k = 0
-            for i in range(m):
-                w_k += x[i, k] * (y[i] - sum(x[i, j] * w[j] for j in range(21) if j != k))
+            w_k = np.transpose(x[:, k]).dot(y - x.dot(w) + w[k] * x[:, k])
             if w_k < -lmb / 2:
                 w_k = (w_k + lmb / 2) / z_k
             elif w_k > lmb / 2:
@@ -115,7 +113,7 @@ def estimate_error(w, data_size):
 
 
 if __name__ == '__main__':
-    test_case = 2
+    test_case = 4
     if test_case == 0:
         X, Y = data_generate(1000)
         w = naive_regression(X, Y, True)
@@ -151,9 +149,9 @@ if __name__ == '__main__':
         plt.plot(l, num_eliminate)
         plt.show()
     elif test_case == 3:
-        iter_lmb = False
+        iter_lmb = True
         if iter_lmb:
-            l = np.linspace(0, 100, 100)
+            l = np.linspace(0, 10, 1000)
             err = []
             for tmp_l in l:
                 X, Y = data_generate(1000)
@@ -163,6 +161,23 @@ if __name__ == '__main__':
             plt.show()
         else:
             X, Y = data_generate(1000)
-            w = lasso_regression(X, Y, 1000000)
+            w = lasso_regression(X, Y, 0.01)
+            print(w)
+    elif test_case == 4:
+        important_features = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 15]
+        iter_lmb = True
+        if iter_lmb:
+            l = np.linspace(0, 0.5, 200)
+            err = []
+            for tmp_l in l:
+                X, Y = data_generate(1000)
+                X_new = np.hstack((X[:, 0:9], X[:, 10:12], X[:, 15:16]))
+                w = ridge_regression(X, Y, True, tmp_l)
+                err.append(estimate_error(w, 10000))
+            plt.plot(l, err)
+            plt.show()
+        else:
+            X, Y = data_generate(1000)
+            w = ridge_regression(X, Y, True, 0.005)
             print(w)
 
